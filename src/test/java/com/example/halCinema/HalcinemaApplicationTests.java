e63d0bbe8c2ed6509817491c3d91f0ae7d08d483
ruriha <06ruriha@gmail.com>
2024-06-02 23:21:35 +0900

    プロジェクトを作成

---
+++ src/test/java/com/example/halCinema/HalcinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.halCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalcinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
