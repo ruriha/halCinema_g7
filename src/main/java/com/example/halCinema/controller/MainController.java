ddacf30d7f894efddd7e0599ac64cc9ca0856cfa
ruriha <06ruriha@gmail.com>
2024-07-01 16:17:05 +0900

    見た目

--- src/main/java/com/example/halCinema/controller/MainController.java
+++ src/main/java/com/example/halCinema/controller/MainController.java
@@ -245,6 +245,8 @@ public class MainController {
         model.addAttribute("selectedCell2Content", selectedCell2Content);
         model.addAttribute("screeningScheduleId", screeningScheduleId);
         model.addAttribute("memberId", memberId);
+//      戻るボタン
+        model.addAttribute("back", "/reserve?screeningScheduleId="+screeningScheduleId);
 	    return "rsv_cfm"; 
 	  }	
 	  
