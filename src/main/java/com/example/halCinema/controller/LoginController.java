d2b6dd72fd0a3d54a8eed0081f60c0b6f6869203
ruriha <06ruriha@gmail.com>
2024-09-13 12:32:22 +0900

    ログイン機能適用

--- src/main/java/com/example/halCinema/controller/LoginController.java
+++ src/main/java/com/example/halCinema/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.halCinema.form.LoginForm;
 import com.example.halCinema.service.LoginService;
+import com.example.halCinema.service.MemberService;
 import com.example.halCinema.service.NewsService;
 
 import jakarta.servlet.http.HttpSession;
@@ -20,12 +21,14 @@ import jakarta.servlet.http.HttpSession;
 public class LoginController {
 
     private final LoginService service;
+    private final MemberService MemberService;
     private final PasswordEncoder passwordEncoder;
     private final MessageSource messageSource;
     private final NewsService newsService; // NewsServiceのインジェクション
 
-    public LoginController(LoginService service, PasswordEncoder passwordEncoder, MessageSource messageSource, NewsService newsService) {
+    public LoginController(LoginService service, MemberService MemberService, PasswordEncoder passwordEncoder, MessageSource messageSource, NewsService newsService) {
         this.service = service;
+        this.MemberService = MemberService;
         this.passwordEncoder = passwordEncoder;
         this.messageSource = messageSource;
         this.newsService = newsService; // コンストラクタに追加
@@ -48,6 +51,18 @@ public class LoginController {
         var userInfo = service.searchUserById(form.getMemberMailaddress());
         var isCorrectUserAuth = userInfo.isPresent() &&
                 passwordEncoder.matches(form.getMemberPassword(), userInfo.get().getMemberPassword());
+        
+        List<Object[]> users = MemberService.loginEntry(form.getMemberMailaddress(), userInfo.get().getMemberPassword());
+
+        if (!users.isEmpty()) {
+            Object[] usersElement = users.get(0);
+            Integer userId = (Integer) usersElement[0];
+            
+            // userId が null でないか確認
+            if (userId != null) {
+                session.setAttribute("userId", userId);
+            }
+        }
 
         if (isCorrectUserAuth) {
             session.setAttribute("loggedInUserEmail", form.getMemberMailaddress());
