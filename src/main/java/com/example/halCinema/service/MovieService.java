25ba8ad24a7adf1daac9b9ba42134a6bbf750ea6
ruriha <06ruriha@gmail.com>
2024-09-30 15:43:52 +0900

    不具合修正

--- src/main/java/com/example/halCinema/service/MovieService.java
+++ src/main/java/com/example/halCinema/service/MovieService.java
@@ -40,15 +40,13 @@ public class MovieService {
     }
     
 
-
-    
     
     //  タイトルから上映時間を取得
     public List<Object[]> findRunningTime(String movieTitle) {
         return MovieRepository.findRunningTime(movieTitle);
     }
 
-}
+
 
 	// 三次開発用 //////////////////////////////////////////////////////////////////////
     
